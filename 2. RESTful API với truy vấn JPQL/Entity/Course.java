378b4ccd6d6b16c3072ea44b3ff039b4ed85a1e4
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-30 09:06:25 +0700

    Create Course.java

---
+++ 2. RESTful API với truy vấn JPQL/Entity/Course.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private Integer duration;
+
+    public Course() {}
+
+    public Course(String title, Integer duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public Integer getDuration() { return duration; }
+    public void setDuration(Integer duration) { this.duration = duration; }
+}
