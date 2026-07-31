d3d86e374c23e0a4274aada44f1077dbada13450
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 09:49:37 +0700

    Delete 4. RESTful API quản lý quan hệ Many-to-Many/Repository/StudentRepository.java

--- 4. RESTful API quản lý quan hệ Many-to-Many/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
-}
